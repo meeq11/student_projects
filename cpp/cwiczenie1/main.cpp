3e1656f90927a7c08447ed83931c60644da52acd
meeq11 <student@student.machine>
2024-09-19 06:01:05 +0000

    studentmachine send

---
+++ cpp/cwiczenie1/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+void babel (vector<int>& vec) {
+	int n = vec.size():
+	bool swapped;
+
+	do {
+		swapped = false;
+		for (int i; i < n - 1; i++) {
+ 			if (vec[i] > vec[i + 1] {
+				swap(vec[i], vec[i + 1]);
+				swapped = true;
+			}
+		}
+		n--;
+		}
+}
+
+
+
+int main(){
+	cout<<"witaj swiecie"<<endl;
+	return 0;
+}
