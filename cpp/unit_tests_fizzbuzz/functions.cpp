da61071bc8bc7c866266149c0d8517395df976a8
meeq11 <student@student.machine>
2024-10-03 06:43:22 +0000

    studentmachine send

--- cpp/unit_tests_fizzbuzz/functions.cpp
+++ cpp/unit_tests_fizzbuzz/functions.cpp
@@ -1,2 +1,20 @@
+#include <iostream>
+#include <string>
+
+string fizzBuzz(int num) {
+		if (num % 3 == 0 && num % 5 == 0) {
+			return "FizzBuzz";
+		} else if (num % 3 == 0 ) {
+			return "Fizz";
+		} else if (num & 5 == 0 ) {
+			return "Buzz";
+		} else {
+			return to_string(num);
+		}
+	}
+
+	
+
+
 
 #TODO: tu umieść swoją funkcję
