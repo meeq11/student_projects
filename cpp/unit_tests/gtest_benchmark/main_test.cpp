3b266168cb5f62c0447e9dd34a65521a0c074129
meeq11 <166530977+meeq11@users.noreply.github.com>
2024-10-17 08:26:40 +0200

    Update main_test.cpp

--- cpp/unit_tests/gtest_benchmark/main_test.cpp
+++ cpp/unit_tests/gtest_benchmark/main_test.cpp
@@ -1,56 +1,71 @@
 #include "benchmark/benchmark.h"
+#include <vector>
 
-/////////////////////////////////////////////////////
-// Funkcja dodająca dwie liczby i jej benchmark
 
-// Funkcja dodająca dwie liczby
-int add(int a, int b){
-	return a+b;
+
+int add(int a, int b) {
+    return a + b;
 }
 
-// benchmark
 static void BM_Add(benchmark::State& state) {
     int a = 5;
     int b = 10;
     for (auto _ : state) {
-        // Benchmarkowany kod
         int result = add(a, b);
         benchmark::DoNotOptimize(result);
     }
 }
 
-//////////////////////////////////////////////////////
-//Funkcja obliczająca sumę kwadratów liczb i jej benchmark
 
-// Prosta funkcja obliczająca sumę kwadratów liczb od 1 do n
 unsigned long long calculateSumOfSquares(int n) {
-    // Alokacja dynamicznej tablicy dla przechowywania kwadratów
     unsigned long long *squares = new unsigned long long[n];
-    // Obliczenie kwadratów i sumy
     unsigned long long sum = 0;
     for (int i = 1; i <= n; ++i) {
         squares[i - 1] = i * i;
         sum += squares[i - 1];
     }
-    // Zwolnienie zaalokowanej pamięci
     delete[] squares;
     return sum;
 }
 
-// benchmark
 static void BM_calculateSumOfSquares(benchmark::State& state) {
     for (auto _ : state) {
-        // Benchmarkowany kod
-        int result = calculateSumOfSquares(5);
+        unsigned long long result = calculateSumOfSquares(5);
         benchmark::DoNotOptimize(result);
     }
 }
 
-/////////////////////////////////////////////////////
-//uruchomienie benchmarków
+
+
+std::vector<int> findPrimes(int n) {
+    std::vector<int> primes;
+    for (int i = 2; i <= n; ++i) {
+        bool isPrime = true;
+        for (int j = 2; j * j <= i; ++j) {
+            if (i % j == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+        if (isPrime) {
+            primes.push_back(i);
+        }
+    }
+    return primes;
+}
+
+static void BM_findPrimes(benchmark::State& state) {
+    int n = 1000;  // Można zwiększyć dla trudniejszych testów
+    for (auto _ : state) {
+        auto primes = findPrimes(n);
+        benchmark::DoNotOptimize(primes);
+    }
+}
+
+
 
 BENCHMARK(BM_Add);
 BENCHMARK(BM_calculateSumOfSquares);
+BENCHMARK(BM_findPrimes);
 
 BENCHMARK_MAIN();
-
